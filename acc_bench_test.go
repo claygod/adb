b86d2e07c9e412cdaef76c2678da53603b3d54ee
claygod <claygod@yandex.ru>
2018-05-28 22:52:19 +0300

    Test fsync

--- acc_bench_test.go
+++ acc_bench_test.go
@@ -5,7 +5,9 @@ package adb
 // Copyright © 2017-2018 Eduard Sesigin. All rights reserved. Contacts: <claygod@yandex.ru>
 
 import (
-	//"fmt"
+	// "fmt"
+	"os"
+	"strconv"
 	"sync"
 	"testing"
 	"time"
@@ -13,22 +15,197 @@ import (
 	"github.com/claygod/transaction"
 )
 
-/*
-func BenchmarkBucketAddQuerySequence(b *testing.B) {
+func BenchmarkFsyncSequense(b *testing.B) {
 	b.StopTimer()
-	bkt := NewBucket()
-	cnt := 1000000
-	qArray := ForTestGenQueryArray(cnt)
+	text := ForTestGenStringArray(256)
+	fv, err := os.OpenFile("benchFsync.txt", os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		panic(err)
+
+	}
+	defer func() {
+		fv.Close()
+		os.Remove("./benchFsync.txt")
+	}()
+
 	b.StartTimer()
 	for i := 0; i < b.N; i++ {
-		if i == cnt {
-			return
-		}
-		//go r.DoTransaction(tArray[i], aArray[i])
-		bkt.AddQuery(qArray[i])
+		fv.WriteString(text)
+		fv.Sync()
 	}
 }
-*/
+
+func BenchmarkFsync16Sequense(b *testing.B) {
+	b.StopTimer()
+	fvs := make([]*os.File, 0, 16)
+
+	for i := 0; i < 16; i++ {
+		fv, _ := os.OpenFile("./benchFsync"+strconv.Itoa(i)+".txt", os.O_CREATE|os.O_WRONLY, 0666)
+		fvs = append(fvs, fv)
+	}
+	text := ForTestGenStringArray(256)
+
+	defer func(fvs []*os.File) {
+		for i := 0; i < 16; i++ {
+			fvs[i].Close()
+			os.Remove("./benchFsync" + strconv.Itoa(i) + ".txt")
+		}
+	}(fvs)
+
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		key := byte(i) >> 4
+		fvs[key].WriteString(text)
+		fvs[key].Sync()
+	}
+}
+
+func BenchmarkFsync2Parallel(b *testing.B) {
+	b.StopTimer()
+	fvs := make([]*os.File, 0, 2)
+
+	for i := 0; i < 2; i++ {
+		fv, _ := os.OpenFile("./benchFsync"+strconv.Itoa(i)+".txt", os.O_CREATE|os.O_WRONLY, 0666)
+		fvs = append(fvs, fv)
+	}
+	text := ForTestGenStringArray(256)
+
+	defer func(fvs []*os.File) {
+		for i := 0; i < 2; i++ {
+			fvs[i].Close()
+			os.Remove("./benchFsync" + strconv.Itoa(i) + ".txt")
+		}
+	}(fvs)
+
+	i := 0
+	b.StartTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			key := ((byte(i)) << 7) >> 7
+			fvs[key].WriteString(text)
+			fvs[key].Sync()
+			i++
+		}
+	})
+}
+
+func BenchmarkFsync4Parallel(b *testing.B) {
+	b.StopTimer()
+	fvs := make([]*os.File, 0, 4)
+
+	for i := 0; i < 4; i++ {
+		fv, _ := os.OpenFile("./benchFsync"+strconv.Itoa(i)+".txt", os.O_CREATE|os.O_WRONLY, 0666)
+		fvs = append(fvs, fv)
+	}
+	text := ForTestGenStringArray(256)
+
+	defer func(fvs []*os.File) {
+		for i := 0; i < 4; i++ {
+			fvs[i].Close()
+			os.Remove("./benchFsync" + strconv.Itoa(i) + ".txt")
+		}
+	}(fvs)
+
+	i := 0
+	b.StartTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			key := ((byte(i)) << 6) >> 6
+			fvs[key].WriteString(text)
+			fvs[key].Sync()
+			i++
+		}
+	})
+}
+
+func BenchmarkFsync8Parallel(b *testing.B) {
+	b.StopTimer()
+	fvs := make([]*os.File, 0, 8)
+
+	for i := 0; i < 8; i++ {
+		fv, _ := os.OpenFile("./benchFsync"+strconv.Itoa(i)+".txt", os.O_CREATE|os.O_WRONLY, 0666)
+		fvs = append(fvs, fv)
+	}
+	text := ForTestGenStringArray(256)
+
+	defer func(fvs []*os.File) {
+		for i := 0; i < 8; i++ {
+			fvs[i].Close()
+			os.Remove("./benchFsync" + strconv.Itoa(i) + ".txt")
+		}
+	}(fvs)
+
+	i := 0
+	b.StartTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			key := ((byte(i)) << 5) >> 5
+			fvs[key].WriteString(text)
+			fvs[key].Sync()
+			i++
+		}
+	})
+}
+
+func BenchmarkFsync16Parallel(b *testing.B) {
+	b.StopTimer()
+	fvs := make([]*os.File, 0, 16)
+
+	for i := 0; i < 16; i++ {
+		fv, _ := os.OpenFile("./benchFsync"+strconv.Itoa(i)+".txt", os.O_CREATE|os.O_WRONLY, 0666)
+		fvs = append(fvs, fv)
+	}
+	text := ForTestGenStringArray(256)
+
+	defer func(fvs []*os.File) {
+		for i := 0; i < 16; i++ {
+			fvs[i].Close()
+			os.Remove("./benchFsync" + strconv.Itoa(i) + ".txt")
+		}
+	}(fvs)
+
+	i := 0
+	b.StartTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			key := ((byte(i)) << 4) >> 4
+			fvs[key].WriteString(text)
+			fvs[key].Sync()
+			i++
+		}
+	})
+}
+
+func BenchmarkFsync32Parallel(b *testing.B) {
+	b.StopTimer()
+	fvs := make([]*os.File, 0, 32)
+
+	for i := 0; i < 32; i++ {
+		fv, _ := os.OpenFile("./benchFsync"+strconv.Itoa(i)+".txt", os.O_CREATE|os.O_WRONLY, 0666)
+		fvs = append(fvs, fv)
+	}
+	text := ForTestGenStringArray(256)
+
+	defer func(fvs []*os.File) {
+		for i := 0; i < 32; i++ {
+			fvs[i].Close()
+			os.Remove("./benchFsync" + strconv.Itoa(i) + ".txt")
+		}
+	}(fvs)
+
+	i := 0
+	b.StartTimer()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			key := ((byte(i)) << 3) >> 3
+			fvs[key].WriteString(text)
+			fvs[key].Sync()
+			i++
+		}
+	})
+}
+
+// ------------------
 
 func BenchmarkDoTransactionParallel(b *testing.B) {
 	b.StopTimer()
@@ -141,6 +318,15 @@ func Benchmark11Sequence(b *testing.B) {
 	}
 }
 */
+func ForTestGenStringArray(num int) string {
+	str := "12345678:879932921731971397821:RTFTRTGGH:+:1230\r\n"
+	var out string
+	for i := 0; i < num; i++ {
+		out += str
+	}
+	return out
+}
+
 func ForTestExeTransaction(r *Reception, t *Transaction, wg *sync.WaitGroup) {
 	r.ExeTransaction(t)
 	wg.Done()
