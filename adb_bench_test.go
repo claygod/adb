d325d968332e64f389306590a81ed8fba0eb4064
claygod <claygod@yandex.ru>
2018-06-16 19:01:46 +0300

    Mini

--- adb_bench_test.go
+++ adb_bench_test.go
@@ -66,7 +66,7 @@ func BenchmarkTransactionParallel(b *testing.B) { // GOGC=off go test -bench=Ben
 	})
 	i := 0
 	// runtime.GOMAXPROCS(1)
-	b.SetParallelism(12)
+	b.SetParallelism(4)
 
 	b.StartTimer()
 
