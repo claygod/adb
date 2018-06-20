49b95cac5227c563f77ef392818f4988225cf1bf
claygod <claygod@yandex.ru>
2018-06-20 13:50:05 +0300

    Update batchqueue

--- batchqueue/bq_api.go
+++ batchqueue/bq_api.go
@@ -4,7 +4,8 @@ package batchqueue
 // API
 // Copyright © 2016 Eduard Sesigin. All rights reserved. Contacts: <claygod@yandex.ru>
 import (
-	"runtime"
+	//"runtime"
+	"fmt"
 	"sync/atomic"
 )
 
@@ -12,13 +13,19 @@ import (
 
 const countBatches uint64 = 256
 
+const (
+	codeDoneNow = iota
+	codeDoneBefore
+	codeNotDone
+)
+
 // const sizeBatch int64 = 1024
 
 type Batchqueue struct {
-	cursor      uint64 // младшая tail, старшая head
-	counterPush uint64
-	counterHead uint64
-	counterTail uint64
+	cursor uint64 // младшая tail, старшая head
+	//counterPush uint64
+	//counterHead uint64
+	//counterTail uint64
 	//batch     *Batch
 	batches   [countBatches]*Batch
 	sizeBatch int64
@@ -27,54 +34,90 @@ type Batchqueue struct {
 // New - create new Batchqueue.
 func New(size int64) *Batchqueue {
 	q := &Batchqueue{sizeBatch: size}
+	q.cursor = 2 // !!! важно
 	for i := uint64(0); i < countBatches; i++ {
 		q.batches[i] = newBatch(size)
 	}
 	return q
 }
 
-func (q *Batchqueue) incrementTail() bool {
+func (q *Batchqueue) incrementTail(cursorTailCur uint8) int {
 	for {
 		oldCursor := atomic.LoadUint64(&q.cursor)
 		cursorHead := uint8(oldCursor >> 32)
 		cursorTail := uint8(oldCursor) + 1
-		delta := int(cursorTail) - int(cursorHead)
-		if 1 < delta || delta < -10 {
+
+		if cursorTailCur != cursorTail-1 {
+			fmt.Println("TAIL: ", cursorTailCur, cursorTail)
+			return codeDoneBefore
+		}
+
+		delta := int(cursorHead) - int(cursorTail)
+		fmt.Println("Delta: ", delta)
+		if -2 < delta && 10 > delta {
+
 			// if cursorTail < cursorHead && cursorTail+10 >= cursorHead{
-			return false
+			return codeNotDone
 		}
 		newCursor := uint64(cursorHead)<<32 + uint64(cursorTail)
 		if atomic.CompareAndSwapUint64(&q.cursor, oldCursor, newCursor) {
-			return true
+			return codeDoneNow
 		}
 	}
 }
 
-func (q *Batchqueue) incrementHead() bool {
+func (q *Batchqueue) incrementHead(cursorHeadCur uint8) int {
 	// голова может толкать (стдвигать хвост- с ограничениями а хвост толкать не может
 	for {
+		var newCursor uint64
 		oldCursor := atomic.LoadUint64(&q.cursor)
 		cursorHead := uint8(oldCursor >> 32)
 		cursorTail := uint8(oldCursor)
+		if cursorHeadCur != cursorHead {
+			return codeDoneBefore
+		}
 		//delta := int(cursorTail+1) - int(cursorHead)
 		if cursorHead+1 == cursorTail {
 			// if cursorTail < cursorHead && cursorTail+10 >= cursorHead{
-			return false
+			//return codeNotDone
+			newCursor = uint64(cursorHead+1)<<32 + uint64(cursorTail+1)
 		}
-		newCursor := uint64(cursorHead)<<32 + uint64(cursorTail)
+		newCursor = uint64(cursorHead+1)<<32 + uint64(cursorTail)
 		if atomic.CompareAndSwapUint64(&q.cursor, oldCursor, newCursor) {
-			return true
+			return codeDoneNow
 		}
 	}
 }
 
 func (q *Batchqueue) Push(item interface{}) bool {
-	for {
-		curCounterTail := atomic.LoadUint64(&q.counterTail)
-		if q.batches[uint8(curCounterTail)].add(item) {
+	for u := 0; u < 5; u++ {
+		//curCounterTail := atomic.LoadUint64(&q.counterTail)
+
+		oldCursor := atomic.LoadUint64(&q.cursor)
+		//cursorHead := uint8(oldCursor >> 32)
+		cursorTail := uint8(oldCursor)
+
+		if q.batches[cursorTail].add(item) {
 			return true
+		} else {
+			//if !q.incrementTail() {
+			//	return false
+			//}
+			fmt.Println("No-add-item ", item)
+			switch q.incrementTail(cursorTail) {
+			case codeDoneBefore:
+				fmt.Println("A codeDoneBefore")
+				continue
+			case codeDoneNow:
+				fmt.Println("A codeDoneNow")
+				//q.batches[cursorHead-1] = newBatch(q.sizeBatch) // +countBatches/2
+				continue //q.batches[cursorHead]
+			case codeNotDone:
+				fmt.Println("A codeNotDone")
+				return false //nil
+			}
 		}
-		runtime.Gosched()
+		//	runtime.Gosched()
 		/*
 			curCounterHead := atomic.LoadUint64(&q.counterHead)
 			if curCounterTail >= curCounterTail+countBatches-5 {
@@ -86,14 +129,35 @@ func (q *Batchqueue) Push(item interface{}) bool {
 			}
 		*/
 	}
+	return false
 }
 
 func (q *Batchqueue) PopBatch() *Batch {
-	curCounter := atomic.AddUint64(&q.counterHead, 1)
-	atomic.StoreInt64(&q.batches[curCounter-1].limit, 0)
-	atomic.AddUint64(&q.counterTail, 1)
-	q.batches[uint8(curCounter-2)] = newBatch(q.sizeBatch) // +countBatches/2
-	return q.batches[curCounter-1]
+	for {
+		oldCursor := atomic.LoadUint64(&q.cursor)
+		cursorHead := uint8(oldCursor >> 32)
+		//cursorTail := uint8(oldCursor)
+		switch q.incrementHead(cursorHead) {
+		case codeDoneBefore:
+			continue
+		case codeDoneNow:
+			q.batches[cursorHead-1] = newBatch(q.sizeBatch) // +countBatches/2
+			return q.batches[cursorHead]
+			//case codeNotDone:
+			//	return nil
+		}
+	}
+
+	//if !q.incrementHead(cursorHead) {
+	//	return nil
+	//}
+	/*
+		curCounter := atomic.AddUint64(&q.counterHead, 1)
+		atomic.StoreInt64(&q.batches[curCounter-1].limit, 0)
+		atomic.AddUint64(&q.counterTail, 1)
+		q.batches[uint8(curCounter-2)] = newBatch(q.sizeBatch) // +countBatches/2
+		return q.batches[curCounter-1]
+	*/
 }
 
 type Batch struct {
--- batchqueue/bq_test.go
+++ batchqueue/bq_test.go
@@ -29,7 +29,37 @@ func TestPush(t *testing.T) {
 	}
 	q.Push(20)
 	q.Push(30)
-	if q.batches[1].data[0] != 30 {
-		t.Error("Error Push|Batch ", q.batches[1])
+	if q.batches[3].data[0] != 30 {
+		t.Error("Error Push|Batch ", q.batches[2])
+	}
+}
+
+func TestPopBatch(t *testing.T) {
+	q := New(2)
+	if !q.Push(10) {
+		t.Error("Error Push")
+	}
+	q.Push(20)
+	q.Push(30)
+	btch := q.PopBatch()
+	if btch == nil {
+		t.Error("Error PopBatch NIL ")
+	}
+
+	if btch.size != 0 {
+		t.Error("Error PopBatch .size ", btch.size)
+	}
+
+	btch = q.PopBatch()
+	//fmt.Println(q.cursor, q.PopBatch())
+	btch = q.PopBatch()
+	//btch = q.PopBatch()
+	if btch.size != 2 {
+		t.Error("Error PopBatch .size ", btch.size)
+		// q.batches[0], q.batches[1], q.batches[2], q.batches[3], q.batches[4], q.batches[5]
+	}
+
+	if btch.data[0] != 10 {
+		t.Error("Error PopBatch .data[0] ", btch.data[0])
 	}
 }
